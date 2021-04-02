c94775d4c0980fec2d1b8eb8a879e371dbbce73b
seulgi <jsg148940@gmail.com>
2021-04-02 20:06:29 +0900

    UI 참고 수정

--- RTD/Assets/GraphicDatas/Animation/test11.cs
+++ RTD/Assets/GraphicDatas/Animation/test11.cs
@@ -7,7 +7,7 @@ public class test11 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 수정
     }
 
     // Update is called once per frame
