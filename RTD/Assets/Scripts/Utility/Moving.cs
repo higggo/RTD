a4f5fc81b1e98a2052a2c5d0f2549f6d014b1270
Hyun-0723 <chsou1702@gmail.com>
2021-04-05 19:22:44 +0900

    n초당 캐릭터 생성 & 목적지 도달시 삭제
    캐릭터 이동 경로 복잡화 및 캐릭터 생성과 목적지 도달시 오브젝트 삭제를 구현했으나 삭제됨에도 불구하고 클론 오브젝트가 잔여하고 있습니다. 추후 수정예정.

--- RTD/Assets/Scripts/Utility/Moving.cs
+++ RTD/Assets/Scripts/Utility/Moving.cs
@@ -7,18 +7,25 @@ public class Moving : MonoBehaviour
     public Vector3[] wayPoints;     //이동할 포인트들을 배열에 저장
     public Vector3 CurrentPosition; //현재위치
     private int wayPointIndex = 0;  //이동 포인트 인덱스
-    private float speed = 4f;       //속도
+    private float speed = 10f;       //속도 (캐릭터 머릿수가 좀 되기때문에 속도를 높였습니다.)
 
 
     // Start is called before the first frame update
     void Start()
     {
-        wayPoints = new Vector3[4];
+        wayPoints = new Vector3[11];                //현재로서는 조금 빙빙 돌아가게끔 디자인 되어 있습니다.
 
-        wayPoints.SetValue(new Vector3(7, 1, 7), 0);
-        wayPoints.SetValue(new Vector3(7, 1, -7), 1);
-        wayPoints.SetValue(new Vector3(-7, 1, -7), 2);
-        wayPoints.SetValue(new Vector3(-7, 1, 7), 3);
+        wayPoints.SetValue(new Vector3(9, 1, -9), 0);
+        wayPoints.SetValue(new Vector3(3, 1, -9), 1);
+        wayPoints.SetValue(new Vector3(3, 1, 9), 2);
+        wayPoints.SetValue(new Vector3(-3, 1, 9), 3);
+        wayPoints.SetValue(new Vector3(-3, 1, -9), 4);
+        wayPoints.SetValue(new Vector3(-9, 1, -9), 5);
+        wayPoints.SetValue(new Vector3(-9, 1, -3), 6);
+        wayPoints.SetValue(new Vector3(9, 1, -3), 7);
+        wayPoints.SetValue(new Vector3(9, 1, 3), 8);
+        wayPoints.SetValue(new Vector3(-9, 1, 3), 9);
+        wayPoints.SetValue(new Vector3(-9, 1, 9), 10);
 
     }
 
@@ -35,6 +42,11 @@ public class Moving : MonoBehaviour
             if (Vector3.Distance(wayPoints[wayPointIndex], CurrentPosition) == 0f)
             {
                 wayPointIndex++;
+                if (wayPointIndex == wayPoints.Length)  //목적지에 도착시 에네미 캐릭터를 삭제합니다.
+                {
+                    Destroy(gameObject);
+                    return;
+                }
             }
         }
     }
---
+++ RTD/Assets/Scripts/Utility/WaveSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefeb;
+    public Transform SpawnPoint;
+    public float TimeBetweenWaves = 2.0f;
+
+    private float CountDown = 1f;
+    private int WaveNumber = 1;
+
+    private void Update()
+    {
+        if(CountDown <=0f)
+        {
+            SpawnWaves();
+            CountDown = TimeBetweenWaves;
+        }
+        CountDown -= Time.deltaTime;
+    }
+
+    private void SpawnWaves()
+    {
+        for (int i = 0; i < WaveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+        WaveNumber++;
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefeb, SpawnPoint.position, SpawnPoint.rotation);
+    }
+}
