79d83496b712f9d2a58b1232ff43df52613ca508
seulgi <jsg148940@gmail.com>
2021-04-12 19:56:22 +0900

    메인화면 수정

---
+++ RTD/Assets/Scrips/MainMeun.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMeun : MonoBehaviour
+{
+    public void ChangefirstScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void changeSecondScen()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickNewGame()
+    {
+
+    }
+
+    public void OnClickLoad()
+    {
+
+
+    }
+
+    public void OnClickOption()
+    {
+
+
+    }
+
+    public void OnClickQuit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
