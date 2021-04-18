2bdb293a55dc5f79ba8d30513c2328256266c480
seulgi <jsg148940@gmail.com>
2021-04-18 13:13:25 +0900

    체력바 추가
    체력바

---
+++ RTD/Assets/Scrips/HealthBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider Health;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.F1))
+        {
+            Health.value += Time.deltaTime;
+        }
+    }
+}
