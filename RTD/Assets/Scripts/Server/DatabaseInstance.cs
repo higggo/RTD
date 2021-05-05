a9209ea449c9ebaac1ee0642ac5cdaaad24c9bf9
higggo <higggo@nate.com>
2021-05-05 15:44:12 +0900

    서버 호출 인스턴스 생성 및 테스트 스크립트

---
+++ RTD/Assets/Scripts/Server/DatabaseInstance.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+
+public class DatabaseInstance : MonoBehaviour
+{
+    void Start()
+    {
+        // Get the root reference location of the database.
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        FirebaseDatabase.DefaultInstance
+      .GetReference("CharHP")
+      .GetValueAsync().ContinueWith(task => {
+          if (task.IsFaulted)
+          {
+              // Handle the error...
+              Debug.Log(task);
+
+          }
+          else if (task.IsCompleted)
+          {
+              DataSnapshot snapshot = task.Result;
+              Debug.Log(snapshot);
+              // Do something with snapshot...
+          }
+      });
+    }
+}
