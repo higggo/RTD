a7c41bdad06879fc1f85587cf9497826a0aa7ffb
higggo <higggo@nate.com>
2021-05-05 17:43:11 +0900

    안드로이드 디펜던시 ForceResolve

--- RTD/Assets/Scripts/Server/DatabaseInstance.cs
+++ RTD/Assets/Scripts/Server/DatabaseInstance.cs
@@ -23,7 +23,29 @@ public class DatabaseInstance : MonoBehaviour
           else if (task.IsCompleted)
           {
               DataSnapshot snapshot = task.Result;
-              Debug.Log(snapshot);
+              Debug.Log(snapshot.Value);
+              GameObject.Find("ServerMsg").GetComponent<TMPro.TextMeshProUGUI>().text = snapshot.Value.ToString();
+              // Do something with snapshot...
+          }
+      });
+    }
+    public void refresh()
+    {
+        FirebaseDatabase.DefaultInstance
+      .GetReference("CharHP")
+      .GetValueAsync().ContinueWith(task =>
+      {
+          if (task.IsFaulted)
+          {
+              // Handle the error...
+              Debug.Log(task);
+
+          }
+          else if (task.IsCompleted)
+          {
+              DataSnapshot snapshot = task.Result;
+              Debug.Log(snapshot.Value);
+              GameObject.Find("ServerMsg").GetComponent<TMPro.TextMeshProUGUI>().text = snapshot.Value.ToString();
               // Do something with snapshot...
           }
       });
